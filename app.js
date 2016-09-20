0cbc388049f4629b80e1749e5bb5e5b07b717448
fishelly.xie <420006782@qq.com>
2016-09-20 21:00:44 +0800

    commit blog index .

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var blog = require('./routes/blog');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/blog', users);
+app.use('/blog', blog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/blog.js
+++ routes/blog.js
@@ -5,4 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('Blog/blog-index', { title: 'Express' });
 });
+router.get('/detail/type', function(req, res, next) {
+  res.render('Blog/blog-type-detail', { title: 'Express' });
+});
 module.exports = router;
