93fd304f9f93888be574a50478710a4e758d168d
fishelly.xie <420006782@qq.com>
2016-09-27 21:01:22 +0800

    add Back directory and back-personal.html

--- routes/back.js
+++ routes/back.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+router.get('/personal', function(req, res, next) {
+  res.render('Back/back-personal', { title: 'Express' });
 });
-router.get('/detail/type', function(req, res, next) {
-  res.render('Blog/blog-type-detail', { title: 'Express' });
-});
-router.get('/article', function(req, res, next) {
-  res.render('Blog/blog-article', { title: 'Express' });
-});
+
 module.exports = router;
