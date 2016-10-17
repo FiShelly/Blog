b9eaa6a6cc91cafce104d5b0a691dc196a750b2b
xiefishelly <ddxfc01@outlook.com>
2016-10-17 22:43:34 +0800

    update blog user op.

---
+++ settings.js
@@ -0,0 +1,11 @@
+/**
+ * Created by FiShelly on 2016/10/16.
+ */
+module.exports = {
+    cookieSecret:'myBlog',
+    db:'blog',
+    host:'127.0.0.1',
+    user:"blog_root",
+    pwd:"110210",
+    port:27017
+};
